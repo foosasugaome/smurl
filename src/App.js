aebe66ad329751cc6a096ab1cda7fe7c1b827c83
Norman Teodoro <normanteodoro@hotmail.com>
2022-07-15 12:36:02 -0700

    added paypal donate button for live testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './layout/Layout'
 import Main from './pages/Main'
 import Redirect from './pages/Redirect';
 import About from './pages/About';
+import Paypal from './pages/Paypal';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Routes>
         <Route path='/:id' element={<Redirect />} />
         <Route path='/' element={<Main />} />
-        <Route path='/about' element={<About />} />                
+        <Route path='/about' element={<About />} />       
+        <Route path='/sendmecoffeemoney' element={<Paypal />} />
       </Routes>
     </Layout>
     </BrowserRouter>
---
+++ src/pages/Paypal.jsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion'
+import './About.css'
+
+export default function Paypal () {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ type: 'spring', delay: 0, duration: 1 }}
+        className='fullscreen-container'
+      >
+        <div className='container-main'>
+          <section>
+          Buying me coffee? Here's a 
+          <a href='https://www.paypal.com/paypalme/normanteodoro?country.x=CA&locale.x=en_US' target='_blank' rel='noreferrer'>link!
+          </a>
+          <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="business" value="BBDCR9LMFBRD6" />
+            <input type="hidden" name="no_recurring" value="0" />
+            <input type="hidden" name="item_name" value="This helps pay for maintaining my site!" />
+            <input type="hidden" name="currency_code" value="CAD" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
+          </form>
+
+          </section>
+        </div>
+      </motion.div>
+    </>
+  )
+}
