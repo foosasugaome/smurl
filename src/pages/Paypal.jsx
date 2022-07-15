82df0b118feaec723f993b4f907fcf92e62ab9cf
Norman Teodoro <normanteodoro@hotmail.com>
2022-07-15 13:00:53 -0700

    added paypal donate button

--- src/pages/Paypal.jsx
+++ src/pages/Paypal.jsx
@@ -12,20 +12,21 @@ export default function Paypal () {
       >
         <div className='container-main'>
           <section>
-          Buying me coffee? Here's a 
+          Buy me coffee? 
+          SmURL will always be free for use. But a little donation can really help keep this website alive. Anyting helps! Thank you.
+          {/* Here's a 
           <a href='https://www.paypal.com/paypalme/normanteodoro?country.x=CA&locale.x=en_US' target='_blank' rel='noreferrer'>link!
-          </a>
+          </a> */}
           </section>
+        </div>
             <form action="https://www.paypal.com/donate" method="post" target="_top">
               <input type="hidden" name="business" value="BBDCR9LMFBRD6" />
               <input type="hidden" name="no_recurring" value="0" />
               <input type="hidden" name="item_name" value="This helps pay for maintaining my site!" />
               <input type="hidden" name="currency_code" value="CAD" />
-              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" className='paypal-button' />
               <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
             </form>
-
-        </div>
       </motion.div>
     </>
   )
